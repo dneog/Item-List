091b4c102dea8ea62e82df0c14e853b4d2dad8ea
unknown <debashisneog22@gmail.com>
2023-03-13 10:39:24 +0530

    Item Lister Code

---
+++ index.js
@@ -0,0 +1,5 @@
+const header= document.getElementById("main-header");
+const title= document.getElementById("title");
+header.style.borderBottom='3px solid #000';
+title.style.fontWeight= 'bold';
+title.style.color= '#008000';
